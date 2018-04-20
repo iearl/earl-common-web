f627a4b19a1d4c8b74ec7b2a42eb7996c36628a2
iearl <dc_magaowei@163.com>
2018-04-20 23:28:26 +0800

    test

---
+++ src/main/java/com/ants/web/boss/controller/co/home/Test.java
@@ -0,0 +1,29 @@
+/**  
+
+* <p>Title: Test.java</p>  
+
+* <p>Description: </p>  
+
+* <p>Copyright: Copyright (c) 2018</p>  
+
+* <p>Company: www.baidudu.com</p>  
+
+* @author shenlan  
+
+* @date 2018年4月20日  
+
+* @version 1.0  
+
+*/  
+package com.ants.web.boss.controller.co.home;
+
+/**  
+ * @ClassName: Test  
+ * @Description: TODO(这里用一句话描述这个类的作用)  
+ * @author 马高伟
+ * @date 2018年4月20日  
+ *    
+ */
+public class Test {
+
+}
